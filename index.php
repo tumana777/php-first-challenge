35fe55f394f8af8bbc45e5b80f2859a155196371
tumana777 <tumana991@gmail.com>
2022-08-12 18:21:32 +0400

    styled form

--- index.php
+++ index.php
@@ -25,24 +25,28 @@
 </head>
 <body>
     <div class="container">
-        <form action="/index.php" method="POST" enctype="multipart/form-data">
-            <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" name="name" class="form-control" placeholder="Enter your name" required>
-            </div>
-            <div class="form-group">
-                <label for="lastName">Lastname</label>
-                <input type="text" name="lastName" class="form-control" placeholder="Enter your lastname" required>
-            </div>
-            <input type="file" name="image">
-            <input type="submit" name="submit">
-        </form>
-
-        <?php 
-            if($name && $lastName && $img){
-                echo "<div class='preview'><h1>$name $lastName</h1><img src=$imagePath></div>";
-            }
-        ?>
+        <?php if(empty($_POST)): ?>
+            <form action="/index.php" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" class="form-control" placeholder="Enter your name" required>
+                </div>
+                <div class="form-group">
+                    <label for="lastName">Lastname</label>
+                    <input type="text" name="lastName" class="form-control" placeholder="Enter your lastname" required>
+                </div>
+                <div class="form-group">
+                    <label for="image">Image</label>
+                    <input type="file" name="image" class="form-control">
+                </div>
+                <input type="submit" name="submit" class="submit">
+            </form>
+        <?php endif; ?>
+        
+        <?php if($name && $lastName && $img): ?>
+            <h1><?php echo "$name $lastName" ?></h1>
+            <img src="<?php echo $imagePath ?>" alt="profile-photo">
+        <?php endif; ?>
     </div>
 </body>
 </html>
